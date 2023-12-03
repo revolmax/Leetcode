d98031ede0d73709adbc9aee4edbc2b64be33238
revolmax <43993402+revolmax@users.noreply.github.com>
2023-12-03 16:47:01 +0100

    Update Reverse_words_in_a_string.py

--- Reverse_words_in_a_string.py
+++ Reverse_words_in_a_string.py
@@ -8,6 +8,13 @@ A word is defined as a sequence of non-space characters. The words in s will be 
 Return a string of the words in reverse order concatenated by a single space.
 Note that s may contain leading or trailing spaces or multiple spaces between two words.
 The returned string should only have a single space separating the words. Do not include any extra spaces."""
+""""
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip()
+        words = s.split()
+        words.reverse()
+        """
 class Solution:
     def reverseWords(self, s: str) -> str:
         return ' '.join(s.split()[::-1])
